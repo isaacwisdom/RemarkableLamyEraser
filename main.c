e80be5472b0dfefab8543d6d46fbbd493a2a4969
Isaac Wisdom <isaacwisdom@gmail.com>
2021-07-02 18:40:30 -0700

    inital stages of RM1 support

--- main.c
+++ main.c
@@ -11,11 +11,7 @@
 
 #define PEN_DEVICE      "/dev/input/event1"
 #define TOUCH_DEVICE    "/dev/input/event2"
-#define PRESS_MODE      1 //press and hold mode
-#define TOGGLE_MODE     2 //toggle mode
 
-#define UNDO            1 //double press action: undo button
-#define ERASE_SELECTION 2 //double press action: erase selection button
 
 //const struct input_event tool_touch_off = { .type = EV_KEY, .code = BTN_TOUCH, .value = 0}; //these might be used in the future to improve press and hold mode
 //const struct input_event tool_pen_on = { .type = EV_KEY, .code = BTN_TOOL_PEN, .value = 1}; //used when pen approaches the screen
@@ -38,31 +34,31 @@ void writeTapWithTouch(int fd, int location[2]) {
 
   //this is the minimum (probably) seqeunce of events that must be sent to tap the screen in a location.
   event = (struct input_event) {.type = EV_ABS, .code = ABS_MT_SLOT, .value = 0x7FFFFFFF}; //Use max signed int slot
-  printf("Writing ABS_MT_SLOT: %d\n", event.value);
+  //printf("Writing ABS_MT_SLOT: %d\n", event.value);
   writeEvent(fd, event);
 
   event = (struct input_event) {.type = EV_ABS, .code = ABS_MT_TRACKING_ID, .value = time(NULL)};
-  printf("Writing Tracking ID: %d\n", event.value);
+  //printf("Writing Tracking ID: %d\n", event.value);
   writeEvent(fd, event);
 
   event = (struct input_event) {.type = EV_ABS, .code = ABS_MT_POSITION_X, .value = location[0]};
-  printf("Writing Touch X: %d\n", event.value);
+  //printf("Writing Touch X: %d\n", event.value);
   writeEvent(fd, event);
 
   event = (struct input_event) {.type = EV_ABS, .code = ABS_MT_POSITION_Y, .value = location[1]};
-  printf("Writing Touch Y: %d\n", event.value);
+  //printf("Writing Touch Y: %d\n", event.value);
   writeEvent(fd, event);
 
   event = (struct input_event) {.type = EV_SYN, .code = SYN_REPORT, .value = 1};
-  printf("Writing SYN Report\n");
+  //printf("Writing SYN Report\n");
   writeEvent(fd, event);
 
   event = (struct input_event) {.type = EV_ABS, .code = ABS_MT_TRACKING_ID, .value = -1};
-  printf("Writing Tracking ID: -1\n");
+  //printf("Writing Tracking ID: -1\n");
   writeEvent(fd, event);
 
   event = (struct input_event) {.type = EV_SYN, .code = SYN_REPORT, .value = 1};
-  printf("Writing SYN Report\n");
+  //printf("Writing SYN Report\n");
   writeEvent(fd, event);
 }
 
@@ -84,6 +80,8 @@ void toggleMode(struct input_event ev, int fd) {
         }
       }
 }
+
+
 void pressMode(struct input_event ev, int fd) {
   if (ev.code == BTN_STYLUS) {
       ev.code = BTN_TOOL_RUBBER; //value will follow the button, so we can reuse the message
@@ -91,6 +89,48 @@ void pressMode(struct input_event ev, int fd) {
     }
 }
 
+void toggleModeRM1 (struct input_event ev, int fd) { //TODO: FIX THIS!!!
+  static int toggle = 0;
+  static int actionComplete = 0;
+  if (ev.code == BTN_STYLUS && ev.value == 1) { // change state of toggle on button press
+      toggle = !toggle;
+      actionComplete = 0;
+      printf("toggle: %d \n", toggle);
+    }
+  if (toggle && !actionComplete) {
+        printf("writing eraser on\n");
+        writeTapWithTouch(fd, eraserTouch);
+        writeTapWithTouch(fd, panelTouch);
+        writeTapWithTouch(fd, eraserTouch);
+        writeTapWithTouch(fd, panelTouch);
+        actionComplete = 1;
+          }
+  else if (!actionComplete){
+        printf("writing eraser off\n");
+        writeTapWithTouch(fd, markerTouch);
+        writeTapWithTouch(fd, panelTouch);
+        writeTapWithTouch(fd, markerTouch);
+        writeTapWithTouch(fd, panelTouch);
+        actionComplete = 1;
+    }
+}
+
+void pressModeRM1(struct input_event ev, int fd) {
+  if (ev.code == BTN_STYLUS && ev.value == 1) {
+      printf("writing eraser_tool on...\n");
+      writeTapWithTouch(fd, eraserTouch);
+      writeTapWithTouch(fd, panelTouch);
+      writeTapWithTouch(fd, eraserTouch);
+      writeTapWithTouch(fd, panelTouch);
+    }
+  else if (ev.code == BTN_STYLUS && ev.value == 0) {
+      printf("writing marker tool on...\n");
+      writeTapWithTouch(fd, markerTouch);
+      writeTapWithTouch(fd, panelTouch);
+      writeTapWithTouch(fd, markerTouch);
+      writeTapWithTouch(fd, panelTouch);
+    }
+}
 bool doublePressHandler(struct input_event ev) { //returns true if a double press has happened
   static struct timeval prevTime;
   const double maxDoublePressTime = .5;
@@ -138,9 +178,24 @@ int main(int argc, char *argv[]) {
                 doublePressAction = UNDO;
                 i++; //manually increment i so we skip interpretting the double press action arg
               }
+            else if (!strncmp(argv[i+1], "redo", 4)) {
+                printf("DOUBLE CLICK ACTION: REDO\n");
+                doublePressAction = REDO;
+                i++; //manually increment i so we skip interpretting the double press action arg
+              }
+            else if (!strncmp(argv[i+1], "eraser-tool", 11)) {
+                printf("DOUBLE CLICK ACTION: ERASER TOOL\n");
+                doublePressAction = ERASER_TOOL;
+                i++; //manually increment i so we skip interpretting the double press action arg
+              }
             else if (!strncmp(argv[i+1], "erase-selection", 15)) {
                 printf("DOUBLE CLICK ACTION: ERASE SELECTION\n");
-                doublePressAction = ERASE_SELECTION;
+                doublePressAction = ERASE_SELECTION_TOOL;
+                i++; //manually increment i so we skip interpretting the double press action arg
+              }
+            else if (!strncmp(argv[i+1], "select-tool", 11)) {
+                printf("DOUBLE CLICK ACTION: SELECT TOOL\n");
+                doublePressAction = SELECT_TOOL;
                 i++; //manually increment i so we skip interpretting the double press action arg
               }
             else {
@@ -200,9 +255,27 @@ int main(int argc, char *argv[]) {
               writeTapWithTouch(fd_touch, undoTouch);
               writeTapWithTouch(fd_touch, panelTouch);  //doing it like this ensures that the panel returns to it's starting state
               break;
-            case ERASE_SELECTION :
+            case REDO :
+              printf("writing redo\n");
+              writeTapWithTouch(fd_touch, redoTouch);
+              writeTapWithTouch(fd_touch, panelTouch);
+              writeTapWithTouch(fd_touch, redoTouch);
+              writeTapWithTouch(fd_touch, panelTouch);
+              break;
+            case ERASER_TOOL :
+              printf("writing redo\n");
+              writeTapWithTouch(fd_touch, eraserTouch);
+              writeTapWithTouch(fd_touch, panelTouch);
+              writeTapWithTouch(fd_touch, eraserTouch);
+              writeTapWithTouch(fd_touch, panelTouch);
+              break;
+            case ERASE_SELECTION_TOOL :
               printf("writing erase selection\n");
               printf("TODO: add in ERASE_SELECTION handling.");
+              break;
+            case SELECT_TOOL :
+              printf("writing select tool\n");
+              printf("TODO: add in SELECT_TOOL handling.");
               break;
             }
           }
