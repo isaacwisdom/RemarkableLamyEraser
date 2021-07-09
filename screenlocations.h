76464bc6fd1e24132719304fcfee96708fee7362
Isaac Wisdom <isaacwisdom@gmail.com>
2021-07-08 18:41:06 -0700

    Working undo regardless or orientation - fixes #8

--- screenlocations.h
+++ screenlocations.h
@@ -10,19 +10,43 @@
 #define UNDO                  1 //undo button
 #define REDO                  2 //redo button
 
-//portrait locations
-#define toolX 63 //define one x coordinate for all panel tools
-int panelTouch[2]   = {toolX, 1820};
-int markerTouch[2]  = {toolX, 1683};
-int eraserTouch[2]  = {toolX, 1450};
-int undoTouch[2]    = {toolX, 1220};
-int redoTouch[2]    = {toolX, 1090};
+//Touch Screen Coordinates:
+//Right Hand, Portrait locations
+#define RHtoolX 60 //define one x coordinate for all panel tools
+int RHPpanel[2]   = {RHtoolX, 1820};
+int RHPmarker[2]  = {RHtoolX, 1680};
+int RHPeraser[2]  = {RHtoolX, 1450};
+int RHPselect[2]  = {RHtoolX, 1340};
+int RHPundo[2]    = {RHtoolX, 1220};
+int RHPredo[2]    = {RHtoolX, 1090};
 
-//landscape locations
-#define toolY 60 //define one y coordinate for panel tools
-int LpanelTouch[2]     = {  65, toolY};
-int LnotebookTouch[2]  = {1355, toolY};
-int LnotebookLower[2]  = {1250, 360};
-int LnotebookUpper[2]  = {1130, 360};
+//Right Hand, Landscape locations
+#define RHtoolY 50 //define one y coordinate for panel tools
+int RHLpanel[2]     = {  40,  RHtoolY};
+int RHLmarker[2]    = {  80,  RHtoolY};
+int RHLeraser[2]    = { 430,  RHtoolY};
+int RHLselect[2]    = { 550,  RHtoolY};
+int RHLundo[2]      = { 670,  RHtoolY};
+int RHLredo[2]      = { 800,  RHtoolY};
+
+//Left Hand, Portrait locations
+#define LHtoolX 1360 //define one y coordinate for panel tools
+int LHPpanel[2]     = {LHtoolX,  1820};
+int LHPmarker[2]    = {LHtoolX,  1680};
+int LHPeraser[2]    = {LHtoolX,  1450};
+int LHPselect[2]    = {LHtoolX,  1340};
+int LHPundo[2]      = {LHtoolX,  1220};
+int LHPredo[2]      = {LHtoolX,  1090};
+
+//Left Hand, Landscape locations
+#define LHtoolY 1820 //define one y coordinate for panel tools
+int LHLpanel[2]     = {  40,  LHtoolY};
+int LHLmarker[2]    = {  80,  LHtoolY};
+int LHLeraser[2]    = { 430,  LHtoolY};
+int LHLselect[2]    = { 550,  LHtoolY};
+int LHLundo[2]      = { 670,  LHtoolY};
+int LHLredo[2]      = { 800,  LHtoolY};
+
+//Pen Coordiantes: (currently not needed)
 
 #endif // SCREENLOCATIONS_H
