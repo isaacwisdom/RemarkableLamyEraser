b8969a138dc48df7f1e11fb7497185075f50d4d2
Tony Zorman <soliditsallgood@mailbox.org>
2023-09-09 08:29:31 +0200

    start-qt.sh: Fix env sourcing

--- docker-qt-support/start-qt.sh
+++ docker-qt-support/start-qt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./opt/codex/rm11x/3.1.15/environment-setup-cortexa7hf-neon-remarkable-linux-gnueabi
+. /opt/codex/rm11x/3.1.15/environment-setup-cortexa7hf-neon-remarkable-linux-gnueabi
 exec qtcreator
